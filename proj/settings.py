01cdbc904afde3e60eda5d7f448de60432dc626c
codesignature <codesignature19@gmail.com>
2026-02-14 12:25:50 +0100

    configured vercel

--- proj/settings.py
+++ proj/settings.py
@@ -137,8 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
@@ -181,3 +182,5 @@ if render_url := os.getenv('RENDER_URL'):
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://.*\.onrender\.com$",
 ]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
