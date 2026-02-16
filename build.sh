4abefd37320998fbf6694a453a22c802bf94dd2f
codesignature <codesignature19@gmail.com>
2026-02-16 07:16:39 +0100

    edited build

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-python -m pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+pip install setuptools
+pip install -r requirements.txt
+
 
 # Apply database migrations
+echo "Make Migrations..."
 python manage.py makemigrations
 
 python manage.py migrate
+
+echo "Collect Static..."
+python manage.py collectstatic --noinput
+
+echo "Build process completed!"
